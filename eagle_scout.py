0a543225a935ea0e047291046d9dbab970b5f45f
Invizabel <46697708+Invizabel@users.noreply.github.com>
2023-09-26 08:46:03 -0500

    Add files via upload

--- eagle_scout.py
+++ eagle_scout.py
@@ -1,95 +1,65 @@
 import datetime
-import io
-import multiprocessing
 import os
 import time
+import cv2
 import face_recognition
 from moviepy.editor import VideoFileClip
-from PIL import Image
-
-def convert_files(_,unknown_folder):
-    home = os.path.expanduser("~")
-    video = VideoFileClip(unknown_folder + "/" + _)
-    video.write_gif(f"{home}/unbound_now/{_}.gif",program="ffmpeg")
-
-def process_images(_,__,known_folder,frame_rate):
-    home = os.path.expanduser("~")
-    if f"{home}/unbound_now/{__}".endswith(".gif"):
-        try:
-            image = Image.open(f"{home}/unbound_now/{__}")
-            for ___ in range(image.n_frames):
-                try:
-                    image.seek(___)
-                    buffer = io.BytesIO()
-                    image.save(buffer,format="png")
-                    known_file = face_recognition.load_image_file(known_folder + "/" + _)
-                    unknown_file = face_recognition.load_image_file(buffer)
-                    known_file = face_recognition.face_encodings(known_file)[0]
-                    unknown_file = face_recognition.face_encodings(unknown_file)[0]
-                    result = bool(face_recognition.compare_faces([known_file],unknown_file)[0])
-                    if result:
-                        with open(f"{home}/unbound_now_output/{_} = {__[:-4]}.txt","a") as file:
-                            file.write(str(datetime.timedelta(seconds=int(___ / frame_rate))) + "\n")
-
-                except IndexError:
-                    continue
-
-        except:
-            print(f"ERROR: {__}")
 
 def eagle_scout(known_folder,unknown_folder):
     os.system("clear")
 
     # prep work
     home = os.path.expanduser("~")
-    if not os.path.exists(f"{home}/unbound_now"):
-        os.makedirs(f"{home}/unbound_now")
 
-    if not os.path.exists(f"{home}/unbound_now_output"):
-        os.makedirs(f"{home}/unbound_now_output")
-
-    core_count = multiprocessing.cpu_count()
+    if not os.path.exists(f"{home}/eagle_scout_output"):
+        os.makedirs(f"{home}/eagle_scout_output")
 
     known_files = os.listdir(known_folder)
     unknown_files = os.listdir(unknown_folder)
 
-    p_list = []
-    core_tracker = 0
-    for _ in unknown_files:
-        core_tracker += 1
-        p = multiprocessing.Process(target=convert_files,args=(_,unknown_folder))
-        p_list.append(p)
-        p.start()
-        if core_tracker % core_count == 0:
-            for __ in p_list:
-                __.join()
-
-    for __ in p_list:
-        __.join()
-
     os.system("clear")
     print("running face recognition")
-    unknown_files = os.listdir(f"{home}/unbound_now")
 
-    p_list = []
-    core_tracker = 0
+    known_image_list = []
     start = time.time()
     for _ in known_files:
-        for __ in unknown_files:
-            core_tracker += 1
-            frame_rate = VideoFileClip(f"{home}/unbound_now/{__}").fps
-            p = multiprocessing.Process(target=process_images,args=(_,__,known_folder,frame_rate))
-            p_list.append(p)
-            p.start()
-            if core_tracker % core_count == 0:
-                for __ in p_list:
-                    __.join()
+        known_image = face_recognition.load_image_file(known_folder + "/" + _)
+        known_image = face_recognition.face_encodings(known_image,)[0]
+        known_image_list.append(known_image)
+    for _ in unknown_files:
+        frame_rate = VideoFileClip(unknown_folder + "/" + _).fps
+        duration = int(VideoFileClip(unknown_folder + "/" + _).fps * VideoFileClip(unknown_folder + "/" + _).duration)
+        capture = cv2.VideoCapture(unknown_folder + "/" + _)
+        frame_position = capture.get(cv2.CAP_PROP_POS_FRAMES)
+        frame_list = []
+        while frame_position <= duration:
+            flag,frame = capture.read()
+            if flag:
+                frame_position = capture.get(cv2.CAP_PROP_POS_FRAMES)
+                rgb_frame = frame[:, :, ::-1]
+                frame_list.append(rgb_frame)
+                if len(frame_list) == 30:
+                    print("#",end="")
+                    try:
+                        face_locations = face_recognition.batch_face_locations(frame_list,number_of_times_to_upsample=0)
+                        face_encodings = face_recognition.face_encodings(frame_list,face_locations)
+                        for face_encoding in face_encodings:
+                            result = bool(face_recognition.compare_faces(known_image_list,face_encoding)[0])
+                            if result:
+                                with open(f"{home}/eagle_scout_output/matches.txt","a") as file:
+                                    file.write(str(datetime.timedelta(seconds=int(frame_position / frame_rate))) + "\n")
 
-    for __ in p_list:
-        __.join()
+                    except IndexError:
+                        pass
+
+                    frame_list = []
+
+            else:
+                capture.set(cv2.CAP_PROP_POS_FRAMES,frame_position-1)
 
     end = time.time()
     total_time = end - start
+    print("",end="\n")
     print("done in " + str(datetime.timedelta(seconds=int(total_time / frame_rate))))
 
 os.system("clear")
