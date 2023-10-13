718877b110d90310132a9f236a3ff3186f5ff81e
Invizabel <46697708+Invizabel@users.noreply.github.com>
2023-10-13 14:30:18 -0500

    Add files via upload

--- subdomain_tool.py
+++ subdomain_tool.py
@@ -10,19 +10,19 @@ for file in files:
     data = pd.read_excel(f"dns/{file}")
     parse = re.findall("\S+\.\S+\.\S+",str(data).lower())
     for _ in parse:
-        if not _.endswith(".") and re.search("[a-z]",_.split(".")[0]):
+        if not _.endswith(".") and re.search("^[a-z]",_.split(".")[0]):
             links.append(_.split(".")[0])
 
-init_links = Counter(links).most_common(20)
+init_links = Counter(links).most_common(1000)
 new_links = []
 for link in init_links:
     new_links.append(link[0])
 
 new_links.sort()
-
 result = "["
 for _ in new_links:
     result += f"\"{_}\","
 
 result += "]"
+print(f"total: {len(new_links)}\n")
 print(result)
