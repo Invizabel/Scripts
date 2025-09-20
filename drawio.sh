7e0045e5c1c66d7dc0db9d3e9e3a0ac321946555
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-09-20 01:14:14 -0700

    Add files via upload

--- drawio.sh
+++ drawio.sh
@@ -1,6 +1,6 @@
 sudo apt update
-sudo apt upgrade -y
-sudo apt autoremove -y
-sudo apt clean
+sudo apt upgrade
+sudo apt autoremove
+sudo apt autoclean
 sudo docker pull jgraph/drawio
 sudo docker run -d --name=drawio --restart always -p 80:8080 jgraph/drawio
