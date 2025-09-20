fa1a6a110b6c817f423e98a740991cc2fdd94825
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-09-20 02:49:36 -0700

    Add files via upload

---
+++ ollama.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install curl
+sudo apt autoremove
+sudo apt autoclean
+curl -fsSL https://ollama.com/install.sh | sh
+sudo docker run -d --network=host -v open-webui://app/backend/data -e OLLAMA_BASE_URL=http://127.0.0.1:11434 --name open-webui --restart always ghcr.io/open-webui/open-webui:main
