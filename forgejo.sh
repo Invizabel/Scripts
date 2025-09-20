a733eaa9a8cdfe786819ffbcb8ec144f1a502819
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-09-20 08:30:50 -0700

    Update forgejo.sh

--- forgejo.sh
+++ forgejo.sh
@@ -5,4 +5,3 @@ sudo dpkg-reconfigure unattended-upgrades
 sudo apt autoremove
 sudo apt autoclean
 sudo docker run -d --name=forgejo --restart always -p 80:3000 codeberg.org/forgejo/forgejo:11.0.6
-
