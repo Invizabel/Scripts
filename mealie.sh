1de36f4d3b1930f08f7087507de13f44e18dd025
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-09-19 19:33:25 -0700

    Add files via upload

---
+++ mealie.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt autoremove -y
+sudo apt clean
+sudo docker pull ghcr.io/mealie-recipes/mealie:latest
+sudo docker run -d --name=mealie --restart always -p 80:9000 ghcr.io/mealie-recipes/mealie:latest
