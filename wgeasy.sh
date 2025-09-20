7864ae36f3643c2ef022506425164dd9480d161e
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-09-20 01:44:24 -0700

    Add files via upload

--- wgeasy.sh
+++ wgeasy.sh
@@ -3,4 +3,16 @@ sudo apt upgrade
 sudo apt autoremove
 sudo apt autoclean
 sudo docker pull ghcr.io/wg-easy/wg-easy:latest
-sudo docker run -d --name=wgeasy --restart always -p 51820:51820/udp -p 80:51821/tcp ghcr.io/wg-easy/wg-easy:latest
+sudo docker run -d \
+  --name=wgeasy \
+  -e WG_HOST=#YOUR_EXTERNAL_IP_OR_DOMAIN# \
+  -e PASSWORD=#YOUR_PASSWORD# \
+  -v ~/.wg-easy:/etc/wireguard \
+  -p 51820:51820/udp \
+  -p 51821:51821/tcp \
+  --cap-add=NET_ADMIN \
+  --cap-add=SYS_MODULE \
+  --sysctl="net.ipv4.conf.all.src_valid_mark=1" \
+  --sysctl="net.ipv4.ip_forward=1" \
+  --restart always \
+  ghcr.io/wg-easy/wg-easy:latest
