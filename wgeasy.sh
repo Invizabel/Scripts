45909afda4f1378aee9cadfa95970cc623f47dab
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-09-20 01:52:21 -0700

    Update wgeasy.sh

--- wgeasy.sh
+++ wgeasy.sh
@@ -4,15 +4,21 @@ sudo apt autoremove
 sudo apt autoclean
 sudo docker pull ghcr.io/wg-easy/wg-easy:latest
 sudo docker run -d \
-  --name=wgeasy \
-  -e WG_HOST=#YOUR_EXTERNAL_IP_OR_DOMAIN# \
-  -e PASSWORD=#YOUR_PASSWORD# \
+  --net wg \
+  -e INSECURE=true \
+  --name wg-easy \
+  --ip6 fdcc:ad94:bacf:61a3::2a \
+  --ip 10.42.42.42 \
   -v ~/.wg-easy:/etc/wireguard \
+  -v /lib/modules:/lib/modules:ro \
   -p 51820:51820/udp \
   -p 51821:51821/tcp \
-  --cap-add=NET_ADMIN \
-  --cap-add=SYS_MODULE \
-  --sysctl="net.ipv4.conf.all.src_valid_mark=1" \
-  --sysctl="net.ipv4.ip_forward=1" \
-  --restart always \
-  ghcr.io/wg-easy/wg-easy:latest
+  --cap-add NET_ADMIN \
+  --cap-add SYS_MODULE \
+  --sysctl net.ipv4.ip_forward=1 \
+  --sysctl net.ipv4.conf.all.src_valid_mark=1 \
+  --sysctl net.ipv6.conf.all.disable_ipv6=0 \
+  --sysctl net.ipv6.conf.all.forwarding=1 \
+  --sysctl net.ipv6.conf.default.forwarding=1 \
+  --restart unless-stopped \
+  ghcr.io/wg-easy/wg-easy:15
