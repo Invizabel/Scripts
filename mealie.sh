1e51a01e65798c4ece80a3cd6dd2518e69744c3a
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-09-20 06:13:27 -0700

    Update mealie.sh

--- mealie.sh
+++ mealie.sh
@@ -6,4 +6,3 @@ sudo apt autoremove
 sudo apt autoclean
 sudo docker pull ghcr.io/mealie-recipes/mealie:latest
 sudo docker run -d --name=mealie --restart always -p 80:9000 ghcr.io/mealie-recipes/mealie:latest
-
