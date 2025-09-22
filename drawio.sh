22656df2580fe29b14257846a6e268f26d1d58a2
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-09-22 17:31:27 -0500

    Delete drawio.sh

--- drawio.sh
+++
@@ -1,8 +0,0 @@
-sudo apt update
-sudo apt upgrade
-sudo apt install unattended-upgrades
-sudo dpkg-reconfigure unattended-upgrades
-sudo apt autoremove
-sudo apt autoclean
-sudo docker pull jgraph/drawio
-sudo docker run -d --name=drawio --restart always -p 80:8080 jgraph/drawio
