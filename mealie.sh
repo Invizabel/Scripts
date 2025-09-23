3bb1371fd4e0291111ec14f12e7d18b8e4ca4a83
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-09-23 07:11:31 -0500

    Delete mealie.sh

--- mealie.sh
+++
@@ -1,8 +0,0 @@
-sudo apt update
-sudo apt upgrade
-sudo apt install unattended-upgrades
-sudo dpkg-reconfigure unattended-upgrades
-sudo apt autoremove
-sudo apt autoclean
-sudo docker pull ghcr.io/mealie-recipes/mealie:latest
-sudo docker run -d --name=mealie --restart always -p 80:9000 ghcr.io/mealie-recipes/mealie:latest
