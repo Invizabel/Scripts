5090c872320bd27a3d37a7bfd4650da62c3f12a1
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-09-24 12:54:26 -0500

    Update http-cat.py

--- http-cat.py
+++ http-cat.py
@@ -10,10 +10,11 @@ os.system("sudo apt autoclean")
 os.system("sudo rm -r /var/www/html/*")
 os.system("git clone https://github.com/httpcats/http.cat")
 os.system("npm install next")
-os.system("npx update-browserlist-db@latest")
+os.system("npx update browserlist-db@latest")
 os.system("cd http.cat")
 os.system("npm run build")
 os.system("cd out")
 os.system("sudo cp -r * /var/www/html")
 os.system("sudo systemctl restart nginx")
+
 
