c3598544fd94b7277122478d26abe3bfb8bdbadb
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-10-27 15:32:54 -0500

    Update drawio.py

--- drawio.py
+++ drawio.py
@@ -1,9 +1,3 @@
 import os
 
-os.system("sudo apt update")
-os.system("sudo apt upgrade")
-os.system("sudo apt install unattended-upgrades")
-os.system("sudo dpkg-reconfigure unattended-upgrades")
-os.system("sudo apt autoremove")
-os.system("sudo apt autoclean")
 os.system("sudo docker run -d --name=drawio --restart always -p 80:8080 jgraph/drawio")
