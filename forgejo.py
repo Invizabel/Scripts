be78f4ba4b8d982f5497a283fe61f82207c07730
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-10-27 09:52:05 -0500

    Update forgejo.py

--- forgejo.py
+++ forgejo.py
@@ -6,4 +6,5 @@ os.system("sudo apt install unattended-upgrades")
 os.system("sudo dpkg-reconfigure unattended-upgrades")
 os.system("sudo apt autoremove")
 os.system("sudo apt autoclean")
-os.system("sudo docker run -d --name=forgejo --restart always -p 80:3000 codeberg.org/forgejo/forgejo:11.0.6")
+os.system("sudo docker run -d --name=forgejo --restart always -p 80:3000 codeberg.org/forgejo/forgejo:13")
+
