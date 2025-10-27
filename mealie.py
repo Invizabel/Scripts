7a96278220c7eec79fb8fecd4221b4234b058de0
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-10-27 15:32:28 -0500

    Update mealie.py

--- mealie.py
+++ mealie.py
@@ -1,10 +1,3 @@
 import os
 
-os.system("sudo apt update")
-os.system("sudo apt upgrade")
-os.system("sudo apt install unattended-upgrades")
-os.system("sudo dpkg-reconfigure unattended-upgrades")
-os.system("sudo apt autoremove")
-os.system("sudo apt autoclean")
-os.system("sudo docker pull ghcr.io/mealie-recipes/mealie:latest")
 os.system("sudo docker run -d --name=mealie --restart always -p 80:9000 ghcr.io/mealie-recipes/mealie:latest")
