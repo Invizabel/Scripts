865b3f997c9aa2402ebe41faf49a817ec91573b0
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-10-28 01:18:32 -0500

    Add files via upload

--- mkv2mp4.py
+++ mkv2mp4.py
@@ -5,4 +5,4 @@ files.sort()
 for file in files:
     if file.endswith(".mkv"):
         print(f"Converting {file}")
-        os.system(f"ffmpeg -i {file} -vcodec libx264 -acodec aac -preset placebo {file.replace('.mkv','.mp4')}")
+        os.system(f"ffmpeg -i {file} -c:v libx264 -preset ultrafast -pix_fmt yuv420p {file.replace('.mkv','.mp4')}")
