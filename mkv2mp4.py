3f9a9a64d37714cda6548c982057cc7fcaae67a4
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-10-29 14:18:38 -0500

    Delete mkv2mp4.py

--- mkv2mp4.py
+++
@@ -1,8 +0,0 @@
-import os
-
-files = os.listdir(".")
-files.sort()
-for file in files:
-    if file.endswith(".mkv"):
-        print(f"Converting {file}")
-        os.system(f"ffmpeg -i {file.replace(' ','\\ ')} {file.replace('.mkv','.mp4').replace(' ','\\ ')}")
