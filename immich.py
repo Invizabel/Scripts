0c47706c6289613b2e1e405ef0c64a042b6fd1d9
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-10-29 14:18:25 -0500

    Delete immich.py

--- immich.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.system("sudo apt update")
-os.system("sudo apt git npm nodejs nginx")
-os.system("mkdir ./immich-app")
-os.system("wget -O ./immich-app/docker-compose.yml https://github.com/immich-app/immich/releases/latest/download/docker-compose.yml")
-os.system("wget -O ./immich-app/.env https://github.com/immich-app/immich/releases/latest/download/example.env")
-
-os.system("sudo docker compose up -d")
