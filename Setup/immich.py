f0c07a36efa5e423149f6155e450f7b416bad903
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-11-14 13:57:13 -0600

    Update immich.py

--- Setup/immich.py
+++ Setup/immich.py
@@ -1,7 +1,7 @@
 import os
 
-os.system("mkdir ./immich-app")
-os.system("wget -O ./immich-app/docker-compose.yml https://github.com/immich-app/immich/releases/latest/download/docker-compose.yml")
-os.system("wget -O ./immich-app/.env https://github.com/immich-app/immich/releases/latest/download/example.env")
+os.system("wget -O docker-compose.yml https://github.com/immich-app/immich/releases/latest/download/docker-compose.yml")
+os.system("wget -O .env https://github.com/immich-app/immich/releases/latest/download/example.env")
 
 os.system("sudo docker compose up -d")
+
