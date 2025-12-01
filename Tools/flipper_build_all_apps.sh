9e500346c3a8b515c5c39f7c314438d5a652e3af
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-12-01 15:25:01 -0600

    Update flipper_build_all_apps.sh

--- Tools/flipper_build_all_apps.sh
+++ Tools/flipper_build_all_apps.sh
@@ -26,10 +26,10 @@ find -follow | grep .yml | while IFS= read -r line; do
     if python3 -u ~/Downloads/flipper-application-catalog/tools/bundle.py --nolint $line "bundle_$count.zip" 2>&1 | grep -qi 'error'; then echo $line >> ~/Downloads/qApps/errors.log; continue; fi
     cp bundle_$count.zip temp
     cd temp
-    unzip ~/Downloads/flipper-application-catalog/applications/temp/bundle_$count.zip
-    cd ~/Downloads/flipper-application-catalog/applications/temp/code/
+    unzip ~/Downloads/flipper-application-catalog/applications/Bluetooth/temp/bundle_$count.zip
+    cd ~/Downloads/flipper-application-catalog/applications/Bluetooth/temp/code/
     python3 -m ufbt
-    cp ~/Downloads/flipper-application-catalog/applications/temp/code/dist/*.fap ~/Downloads/qApps/Bluetooth
+    cp ~/Downloads/flipper-application-catalog/applications/Bluetooth/temp/code/dist/*.fap ~/Downloads/qApps/Bluetooth
     rm -rf code
 done
 rm -rf ~/Downloads/flipper-application-catalog
