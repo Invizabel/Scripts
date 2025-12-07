485f68693958e095ede2490f32f56bc44cca1b8b
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-12-06 20:46:57 -0600

    Add files via upload

--- Tools/compile_linux_kernel.py
+++ Tools/compile_linux_kernel.py
@@ -10,6 +10,7 @@ os.chdir("KERNEL")
 
 os.system("sudo apt update")
 os.system("sudo apt install bc binutils bison dwarves flex gcc git gnupg2 gzip libelf-dev libncurses5-dev libssl-dev make openssl pahole perl-base rsync tar xz-utils")
+os.system("sudo apt install libdwarf-dev libelf-dev dwarves")
 os.system(f"wget https://cdn.kernel.org/pub/linux/kernel/v6.x/{kernel_version}.tar.xz")
 os.system(f"tar -xvf {kernel_version}.tar.xz")
 os.chdir(kernel_version)
