53232f97d7a4cc8cc9b89e99fbe1146dbc536fc1
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-12-06 20:44:21 -0600

    Add files via upload

--- Tools/compile_linux_kernel.py
+++ Tools/compile_linux_kernel.py
@@ -4,15 +4,15 @@ if os.path.exists("KERNEL"):
     os.system("rm -rf KERNEL")
 os.mkdir("KERNEL")
 
-fetch_files = "linux-6.18.tar.xz"
+kernel_version = "linux-6.18"
 
 os.chdir("KERNEL")
 
 os.system("sudo apt update")
 os.system("sudo apt install bc binutils bison dwarves flex gcc git gnupg2 gzip libelf-dev libncurses5-dev libssl-dev make openssl pahole perl-base rsync tar xz-utils")
-os.system(f"wget https://cdn.kernel.org/pub/linux/kernel/v6.x/{fetch_files}")
-os.system(f"tar -xvf {fetch_files}")
-os.chdir("linux-*/")
+os.system(f"wget https://cdn.kernel.org/pub/linux/kernel/v6.x/{kernel_version}.tar.xz")
+os.system(f"tar -xvf {kernel_version}.tar.xz")
+os.chdir(kernel_version)
 os.system("make olddefconfig")
 os.system("./scripts/config --file .config --disable MODULE_SIG")
 os.system("make -j$(nproc) 2>&1 | tee log")
