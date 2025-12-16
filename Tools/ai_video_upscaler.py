278690b4f1648f84ce590d9296ccb4758bfa3fa5
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-12-15 21:52:19 -0600

    Add files via upload

--- Tools/ai_video_upscaler.py
+++ Tools/ai_video_upscaler.py
@@ -1,5 +1,3 @@
-# For use with Upscayl or other batching ai upscale program
-
 import cv2
 import os
 
@@ -23,8 +21,8 @@ def video_to_image(filename):
     cap.release()
     print(f"Extracted {frame_idx} frames from {filename}")
 
-def image_to_video(filename):
-    os.system(f"ffmpeg -framerate 29.97 -i OUT/{filename}_frame_%010d.png  output.mp4")
+def image_to_video(filename,framerate):
+    os.system(f"ffmpeg -framerate {framerate} -i OUT/{filename}_frame_%010d.png  output.mkv")
 
 mode = input("1 = Video2Frames | 2 = Frames2Video\n")
 if mode == "1":
@@ -33,4 +31,5 @@ if mode == "1":
 
 if mode == "2":
     filename = input("filename: ")
-    image_to_video(filename)
+    framerate = input("frame rate: ")
+    image_to_video(filename,framerate)
