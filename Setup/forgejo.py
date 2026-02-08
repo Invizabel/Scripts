b10b092577d646763b6dea7755dc13c9b8c2e5c9
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-02-07 20:17:34 -0600

    Update forgejo.py

--- Setup/forgejo.py
+++ Setup/forgejo.py
@@ -1,3 +1,4 @@
 import os
 
-os.system("sudo docker run -d --name=forgejo --restart always -p 80:3000 codeberg.org/forgejo/forgejo:13")
+os.system("sudo docker run -d --name=forgejo --restart always -p 80:3000 codeberg.org/forgejo/forgejo:14.0.2")
+
