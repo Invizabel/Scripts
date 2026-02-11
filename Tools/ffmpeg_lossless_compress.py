7bcd2170d9a42c920374a78530777421c0b3bd27
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-02-10 22:17:35 -0600

    Add files via upload

--- Tools/ffmpeg_lossless_compress.py
+++ Tools/ffmpeg_lossless_compress.py
@@ -2,11 +2,13 @@ import os
 
 files = os.listdir(".")
 files.sort()
+if not os.path.exists("OUT"):
+    os.mkdir("OUT")
 for file in files:
     if file.endswith(".mkv"):
         print(f"Converting {file}")
-        os.system(f"ffmpeg -i {file.replace(' ','\\ ')} -c:v ffv1 -c:a flac {file.replace(' ','\\ ')}")
+        os.system(f"ffmpeg -i {file.replace(' ','\\ ')} -c:v ffv1 -c:a flac OUT/{file.replace(' ','\\ ')}")
 
     if file.endswith(".wav"):
         print(f"Converting {file}")
-        os.system(f"ffmpeg -i {file.replace(' ','\\ ')} {file.replace('.wav','.flac').replace(' ','\\ ')}")
+        os.system(f"ffmpeg -i {file.replace(' ','\\ ')} OUT/{file.replace('.wav','.flac').replace(' ','\\ ')}")
