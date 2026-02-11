431074867bf3e3e4356821e39d643757de09da60
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-02-10 22:27:04 -0600

    Update ffmpeg_lossless_compress.py

--- Tools/ffmpeg_lossless_compress.py
+++ Tools/ffmpeg_lossless_compress.py
@@ -9,9 +9,8 @@ if not os.path.exists("OUT"):
 for file in files:
     if file.endswith(".mkv"):
         print(f"Converting {file}")
-        os.system(f"ffmpeg -i {file.replace(' ','\\ ')} -c:v ffv1 -c:a flac OUT/{file.replace(' ','\\ ')}")
+        os.system(f"ffmpeg -i {file.replace(' ','\\ ')} -c:a flac OUT/{file.replace(' ','\\ ')}")
 
     if file.endswith(".wav"):
         print(f"Converting {file}")
         os.system(f"ffmpeg -i {file.replace(' ','\\ ')} OUT/{file.replace('.wav','.flac').replace(' ','\\ ')}")
-        
