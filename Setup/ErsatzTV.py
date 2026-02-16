9dafbc98e32fb94bc12c18f430b7f6423836ebff
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-02-15 21:09:49 -0600

    Update ErsatzTV.py

--- Setup/ErsatzTV.py
+++ Setup/ErsatzTV.py
@@ -1,3 +1,3 @@
 import os
 
-os.system("docker run -d --name ersatztv -e TZ=America/Chicago -p 80:8409 --restart always ghcr.io/ersatztv/ersatztv")
+os.system("sudo docker run -d --name ersatztv -e TZ=America/Chicago -p 80:8409 --restart always ghcr.io/ersatztv/ersatztv")
