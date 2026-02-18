f843f3883548d0d8e1b6f0ef860060c375ff6465
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-02-18 15:52:58 -0600

    Update ErsatzTV.py

--- Setup/ErsatzTV.py
+++ Setup/ErsatzTV.py
@@ -1,5 +1,9 @@
 import os
 
+os.system("sudo rm -rf ~/config")
 os.system("mkdir ~/config")
 user = input("user:\n")
+os.system("sudo apt remove docker-desktop")
+os.system("sudo apt purge docker-desktop")
+os.system("sudo rm -rf ~/.docker")
 os.system(f"""sudo docker run --runtime=nvidia --gpus all -d --name ersatztv -e TZ=America/Chicago -p 80:8409 -v /home/{user}/Videos/Movies:/media/movies:ro -v /home/{user}/Videos/TV:/media/tv:ro -v /home/{user}/Music:/media/music:ro -v /home/{user}/config:/config --restart always ghcr.io/ersatztv/ersatztv""")
