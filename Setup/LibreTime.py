0628195a8901a1dbf3fa468fabaaa41b3e7f7bfc
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-02-18 10:01:06 -0600

    Update LibreTime.py

--- Setup/LibreTime.py
+++ Setup/LibreTime.py
@@ -6,7 +6,9 @@ os.system("echo LIBRETIME_VERSION="4.5.0" > .env")
 os.system("source .env")
 os.system("wget 'https://raw.githubusercontent.com/libretime/libretime/$LIBRETIME_VERSION/docker-compose.yml'")
 os.system("wget 'https://raw.githubusercontent.com/libretime/libretime/$LIBRETIME_VERSION/docker/config.template.yml'")
-os.system('echo "# Postgres
+import os
+
+os.system("""echo "# Postgres
 POSTGRES_PASSWORD=$(openssl rand -hex 16)
 
 # RabbitMQ
@@ -15,7 +17,8 @@ RABBITMQ_DEFAULT_PASS=$(openssl rand -hex 16)
 # Icecast
 ICECAST_SOURCE_PASSWORD=$(openssl rand -hex 16)
 ICECAST_ADMIN_PASSWORD=$(openssl rand -hex 16)
-ICECAST_RELAY_PASSWORD=$(openssl rand -hex 16)" >> .env"')
+ICECAST_RELAY_PASSWORD=$(openssl rand -hex 16)" >> .env""")
+
 os.system('bash -a -c "source .env; envsubst < config.template.yml > config.yml"')
 os.system("sudo apt update && sudo apt install gettext-base")
 os.system("docker compose run --rm api libretime-api migrate")
