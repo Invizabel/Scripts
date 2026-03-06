f2cd7091318b954840313c4bbd11e1e59f5d8493
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-03-05 22:56:49 -0600

    Add files via upload

--- Tools/ffmpeg_compress.py
+++ Tools/ffmpeg_compress.py
@@ -3,16 +3,14 @@ import os
 files = os.listdir(".")
 files.sort()
 
-hwaccel = input("hardware acceleration: ")
-
 if not os.path.exists("OUT"):
     os.mkdir("OUT")
     
 for file in files:
     if file.endswith(".mkv"):
         print(f"Converting {file}")
-        os.system(f"ffmpeg -hwaccel {hwaccel} -i '{file}' 'OUT/{file.replace('.mkv','.mp4')}'")
+        os.system(f"ffmpeg -i '{file}' -c:a flac 'OUT/{file}'")
 
     if file.endswith(".wav"):
         print(f"Converting {file}")
-        os.system(f"ffmpeg -hwaccel {hwaccel} -i '{file}' 'OUT/{file.replace('.wav','.flac')}'")
+        os.system(f"ffmpeg -i '{file}' 'OUT/{file.replace('.wav','.flac')}'")
