4e383b4a19bf29c4e32f5dbaa803cdd22689b1da
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-03-07 12:39:21 -0600

    Add files via upload

--- Tools/ffmpeg_compress.py
+++ Tools/ffmpeg_compress.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 files = os.listdir(".")
@@ -9,8 +10,20 @@ if not os.path.exists("OUT"):
 for file in files:
     if file.endswith(".mkv"):
         print(f"Converting {file}")
-        os.system(f"ffmpeg -i '{file}' -map 0:v -map 0:a:1 -c:a flac 'OUT/{file}'")
 
-    if file.endswith(".wav"):
-        print(f"Converting {file}")
-        os.system(f"ffmpeg -i '{file}' -c:a flac 'OUT/{file.replace('.wav','.flac')}'")
+        os.system(f"ffprobe -show_streams -print_format json -i '{file}' > out.json")
+        with open("out.json", "r") as f:
+            data = json.loads(f.read())
+
+        best_audio = -1
+        for i in list(data.values())[0]:
+            if i["tags"]["language"] == "eng" and i["codec_type"] == "audio":
+                if i["channel_layout"] != "stereo" and i["channel_layout"] != "mono":
+                    best_audio = i["index"]
+                    break
+
+        if best_audio == -1:
+             os.system(f"ffmpeg -i '{file}' -c:a flac -c:s copy -c:v copy 'OUT/{file}'")
+             
+        else:
+            os.system(f"ffmpeg -i '{file}' -map 0:v:0 -map 0:a:{best_audio} -c:a flac -c:s copy -c:v copy 'OUT/{file}'")
