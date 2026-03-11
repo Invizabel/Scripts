c33eccbcbbcfccafbb708c634444204816026c32
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-03-10 21:50:21 -0500

    Add files via upload

---
+++ Tools/flipper_wav_convert.py
@@ -0,0 +1,23 @@
+# WARNING: MAKE A COPY before using this script, this will rename files if they can't be used correctly.
+
+import os
+
+os.system("ls -R > directory_listing.txt")
+
+if not os.path.exists("OUT"):
+    os.mkdir("OUT")
+    
+folder = ""
+with open("directory_listing.txt", "r") as file:
+    for line in file:
+        if line.startswith("./"):
+            folder = line.strip()[:-1].replace("./", "")
+            if not os.path.exists(f"OUT/{folder}"):
+                os.mkdir(f"OUT/{folder}")
+        else:
+            file = f"{folder}/{line}".strip()
+            if file.endswith(".flac") or file.endswith(".wav"):
+                # force rename removing quotes
+                os.rename(file,file.replace("'","").replace('"',''))
+                print(f"Converting {file.replace('\'','').replace('\"','')}")
+                os.system(f"ffmpeg -i '{file.replace('\'','').replace('\"','')}' -ac 2 -c:a pcm_u8 'OUT/{file.replace('\'','').replace('\"','').replace('.flac', '.wav')}'")
