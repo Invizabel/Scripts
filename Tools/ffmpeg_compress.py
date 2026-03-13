625ac9fd0a4a21db5592a2ac8b89bd609399be85
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-03-13 16:11:25 -0500

    Update ffmpeg_compress.py

--- Tools/ffmpeg_compress.py
+++ Tools/ffmpeg_compress.py
@@ -1,12 +1,29 @@
+# WARNING: MAKE A COPY before using this script, this will rename files if they can't be used correctly.
+
 import os
 
-files = os.listdir(".")
-files.sort()
+os.system("ls -R > directory_listing.txt")
 
 if not os.path.exists("OUT"):
     os.mkdir("OUT")
     
-for file in files:
-    if file.endswith(".mkv"):
-        print(f"Converting {file}")
-        os.system(f"ffmpeg -i '{file}' 'OUT/{file.replace('.mkv', '.mp4')}'")
+folder = ""
+with open("directory_listing.txt", "r") as file:
+    for line in file:
+        if line.startswith("./"):
+            folder = line.strip()[:-1].replace("./", "")
+            if not os.path.exists(f"OUT/{folder}"):
+                os.mkdir(f"OUT/{folder}")
+        else:
+            file = f"{folder}/{line}".strip()
+            if file.endswith(".flac") or file.endswith(".wav"):
+                # force rename removing quotes
+                os.rename(file,file.replace("'","").replace('"',''))
+                print(f"Converting {file.replace('\'','').replace('\"','')}")
+                os.system(f"ffmpeg -i '{file.replace('\'','').replace('\"','')}' 'OUT/{file.replace('\'','').replace('\"','').replace('.flac', '.mp3')}'")
+
+            if file.endswith(".mkv"):
+                # force rename removing quotes
+                os.rename(file,file.replace("'","").replace('"',''))
+                print(f"Converting {file.replace('\'','').replace('\"','')}")
+                os.system(f"ffmpeg -i '{file.replace('\'','').replace('\"','')}' 'OUT/{file.replace('\'','').replace('\"','').replace('.mkv', '.mp4')}'")
