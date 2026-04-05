3ffa701e3df0f492521540d55a94a6193ad1253a
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-04-04 20:24:24 -0500

    Add files via upload

--- Setup/pelican.py
+++ Setup/pelican.py
@@ -2,18 +2,5 @@ import os
 
 host_name = input("host name: ")
 admin_email = input("admin email: ")
-admin_password = input("admin password: ")
 
-os.system(f"sudo docker run -d --name=pelican --restart always "
-          f"-p 80:80 -p 443:443 "
-          f"-v pelican-data:/pelican-data "
-          f"-v pelican-logs:/var/www/html/storage/logs "
-          f"-e XDG_DATA_HOME=/pelican-data "
-          f"-e APP_URL='http://{host_name}' "
-          f"-e ADMIN_EMAIL='{admin_email}' "
-          f"-e PANEL_PASSWORD='{admin_password}' "
-          f"-e PANEL_HOST=0.0.0.0 "
-          f"-e PANEL_PORT=80 "
-          f"-e DB_CONNECTION=sqlite "
-          f"-e DB_DATABASE=/pelican-data/database.sqlite "
-          f"ghcr.io/pelican-dev/panel:main")
+os.system(f'docker run -d --name panel --restart always -p 80:80 -p 443:443 --add-host host.docker.internal:host-gateway -v pelican-data:/pelican-data -v pelican-logs:/var/www/html/storage/logs -e XDG_DATA_HOME=/pelican-data -e APP_URL="http://{host_name}" -e ADMIN_EMAIL="{admin_email}" ghcr.io/pelican-dev/panel:main')
