6a642bfdbe76c04d59f5f84c027b9fb11406655c
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-04-04 19:52:18 -0500

    Add files via upload

--- Setup/pelican.py
+++ Setup/pelican.py
@@ -1,3 +1,3 @@
 import os
 
-os.system("sudo docker run -d --name=pelican --restart always ghcr.io/pelican-dev/panel:main")
+os.system("sudo docker run -d --name=pelican --restart always -p 80:80 -p 443:443 -e PANEL_HOST=0.0.0.0 -e PANEL_PORT=80 ghcr.io/pelican-dev/panel:main")
