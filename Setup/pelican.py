9dda469856a829bc9a3716d5ffc24547ad135ca0
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-04-04 20:28:31 -0500

    Add files via upload

--- Setup/pelican.py
+++ Setup/pelican.py
@@ -3,4 +3,4 @@ import os
 host_name = input("host name: ")
 admin_email = input("admin email: ")
 
-os.system(f'sudo docker run -d --name panel --restart always -p 80:80 -p 443:443 --add-host host.docker.internal:host-gateway -v pelican-data:/pelican-data -v pelican-logs:/var/www/html/storage/logs -e XDG_DATA_HOME=/pelican-data -e APP_URL="http://{host_name}" -e ADMIN_EMAIL="{admin_email}" ghcr.io/pelican-dev/panel:main')
+os.system(f'sudo docker run -d --name pelican --restart always -p 80:80 -p 443:443 --add-host host.docker.internal:host-gateway -v pelican-data:/pelican-data -v pelican-logs:/var/www/html/storage/logs -e XDG_DATA_HOME=/pelican-data -e APP_URL="http://{host_name}" -e ADMIN_EMAIL="{admin_email}" ghcr.io/pelican-dev/panel:main')
